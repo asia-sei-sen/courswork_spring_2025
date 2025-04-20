397c37ae0f2c38615c5be15e2b6ddfea1eeb063d
ASIA_sei_sen <sei_senagon@yahoo.com>
2025-04-20 20:32:34 +0500

    Added unit tests for the masking functionality.

--- test/test_masks.py
+++ test/test_masks.py
@@ -6,7 +6,7 @@ def test_card_masking():
     """Тест маскировки номера карты."""
     assert mask_credit_card("1234567890123456") == "1234 56** **** 3456"
     assert mask_credit_card("1234 5678 9012 3456") == "1234 56** **** 3456"
-    assert mask_credit_card("1234") == "Некорректный номер карты"
+    assert mask_credit_card("1234") == "Некорректный  номер карты"
 
 def test_account_masking():
     """Тест маскировки номера счета."""
