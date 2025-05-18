023fec636a21d1227fe37892f2e6c75ae160cb17
ASIA_sei_sen <sei_senagon@yahoo.com>
2025-05-18 23:26:18 +0500

    Обновление декоратора log: добавлен docstring

--- src/decorators/log.py
+++ src/decorators/log.py
@@ -6,8 +6,29 @@ from functools import (
     wraps,
 )
 
-
 def log(filename=None):
+    """
+    Декоратор для логирования выполнения функции.
+
+    Логирует начало и конец выполнения функции, а также её результат или возникшие ошибки.
+    Логи могут записываться либо в указанный файл, либо выводиться в консоль.
+
+    Args:
+        filename (str, optional): Имя файла для записи логов.
+                                  Если None, логи выводятся в консоль.
+
+    Returns:
+        function: Обернутая функция с логированием.
+
+    Пример использования:
+        @log(filename="log.txt")
+        def add(a, b):
+            return a + b
+
+        add(2, 3)
+        # В log.txt:
+        # [2025-05-20 14:33:01.123456] add ok | args: (2, 3), kwargs: {}, result: 5
+    """
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -28,5 +49,7 @@ def log(filename=None):
                 else:
                     print(log_entry, file=sys.stderr)
                 raise
+
         return wrapper
+
     return decorator
